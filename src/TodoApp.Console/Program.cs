35404b15a13a06e549d1878636bed9a99354846d
Cesar Betancourt <cessarr862@gmail.com>
2025-09-12 14:43:14 -0600

    Phase_1_CS_Syntax_Variables - Cesar Betancourt

--- src/TodoApp.Console/Program.cs
+++ src/TodoApp.Console/Program.cs
@@ -1,30 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TodoApp.Domain;
-using Microsoft.Extensions.DependencyInjection;
-using TodoApp.Application.DTOs;
-using TodoApp.Application.Interfaces;
-using TodoApp.Infrastructure;
+﻿using TodoApp.Domain;
 
 Console.WriteLine("== MyTodo Console ==");
 Console.WriteLine("Type a task title and press Enter (or just Enter to exit).");
 
 var id = 1;
+
 while (true)
 {
     Console.Write("> ");
     var input = Console.ReadLine();
 
-    if (string.IsNullOrWhiteSpace(input))
-        break;
+    if (string.IsNullOrWhiteSpace(input)) break;
 
-    // explicit type for clarity
     Todo newTodo = new(id++, input, null, false);
 
-    // 'var' is fine when the type is obvious from the RHS:
     var message = $"Created: [{newTodo.Id}] {newTodo.Title}";
     Console.WriteLine(message);
 }
--- src/TodoApp.Domain/Todo.cs
+++ src/TodoApp.Domain/Todo.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TodoApp.Domain
+﻿namespace TodoApp.Domain
 {
     public record Todo(int Id, string Title, DateOnly? DueDate, bool IsDone = false);
-    
 }
