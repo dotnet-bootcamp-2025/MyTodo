c192b3496b18e8d2fe9610de4d544ac5c47cf4a7
EliotdelaCruz <encruz646@gmail.com>
2025-09-12 12:22:56 -0600

    Phase_1_CS_Syntax_Variables - Eliot delaCruz

---
+++ src/TodoApp.Domain/Todo_Old.cs
@@ -0,0 +1,57 @@
+namespace TodoApp.Domain.Entities;
+
+
+public class Todo_Old
+{
+    public Guid Id { get; private set; }
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public bool IsCompleted { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? CompletedAt { get; private set; }
+
+    private Todo_Old() { }
+
+    public static Todo_Old Create(string title, string description)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("Title cannot be empty", nameof(title));
+
+        return new Todo_Old
+        {
+            Id = Guid.NewGuid(),
+            Title = title,
+            Description = description ?? string.Empty,
+            IsCompleted = false,
+            CreatedAt = DateTime.UtcNow
+        };
+    }
+
+    public void MarkAsCompleted()
+    {
+        if (!IsCompleted)
+        {
+            IsCompleted = true;
+            CompletedAt = DateTime.UtcNow;
+        }
+    }
+
+    public void MarkAsIncomplete()
+    {
+        if (IsCompleted)
+        {
+            IsCompleted = false;
+            CompletedAt = null;
+        }
+    }
+
+    public void UpdateDetails(string title, string description)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("Title cannot be empty", nameof(title));
+
+        Title = title;
+        Description = description ?? string.Empty;
+    }
+
+}
--- src/TodoApp.Infrastructure/Repositories/InMemoryTodoRepository.cs
+++ src/TodoApp.Infrastructure/Repositories/InMemoryTodoRepository.cs
@@ -6,26 +6,26 @@ using TodoApp.Domain.Repositories;
 
 public class InMemoryTodoRepository : ITodoRepository
 {
-    private readonly ConcurrentDictionary<Guid, Todo> _todos = new();
+    private readonly ConcurrentDictionary<Guid, Todo_Old> _todos = new();
 
-    public Task<IEnumerable<Todo>> GetAllAsync()
+    public Task<IEnumerable<Todo_Old>> GetAllAsync()
     {
-        return Task.FromResult<IEnumerable<Todo>>(_todos.Values.ToList());
+        return Task.FromResult<IEnumerable<Todo_Old>>(_todos.Values.ToList());
     }
 
-    public Task<Todo?> GetByIdAsync(Guid id)
+    public Task<Todo_Old?> GetByIdAsync(Guid id)
     {
         _todos.TryGetValue(id, out var todo);
         return Task.FromResult(todo);
     }
 
-    public Task AddAsync(Todo todo)
+    public Task AddAsync(Todo_Old todo)
     {
         _todos.TryAdd(todo.Id, todo);
         return Task.CompletedTask;
     }
 
-    public Task UpdateAsync(Todo todo)
+    public Task UpdateAsync(Todo_Old todo)
     {
         _todos[todo.Id] = todo;
         return Task.CompletedTask;
