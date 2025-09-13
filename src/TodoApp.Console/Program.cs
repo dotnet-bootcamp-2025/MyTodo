2fab65c1f341ed6d0d4975652b011d23fe299e0e
EliotdelaCruz <encruz646@gmail.com>
2025-09-12 23:29:54 -0600

    Phase_0-Bootstrap_read_run_the_repo - Eliot delaCruz

--- src/TodoApp.Console/Program.cs
+++ src/TodoApp.Console/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.Extensions.DependencyInjection;
 using TodoApp.Application.DTOs;
 using TodoApp.Application.Interfaces;
 using TodoApp.Infrastructure;
-
+//Phase 0 setup complete
 var services = new ServiceCollection();
 services.AddInfrastructure();
 var serviceProvider = services.BuildServiceProvider();
