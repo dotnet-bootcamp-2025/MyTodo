0b95673cac2e9f8bad8511ea485e482166fca62a
Gadiel Mar <gadielmar10@outlook.com>
2025-09-13 13:28:02 -0600

    Phase1 Exercises

---
+++ ConsoleAppPhase1/Program.cs
@@ -0,0 +1,23 @@
+﻿using TodoApp.Domain.Phase1;
+
+Console.WriteLine("== MyTodo Console ==");
+Console.WriteLine("Type a task title and press Enter (or just Enter to exit).");
+
+var id = 1;
+while (true)
+{
+    Console.Write("> ");
+    var input = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(input))
+        break;
+
+    // explicit type for clarity
+    Todo newTodo = new(id++, input, null, false);
+
+    // 'var' is fine when the type is obvious from the RHS:
+    var message = $"Created: [{newTodo.Id}] {newTodo.Title}";
+    Console.WriteLine(message);
+}
+
+Console.WriteLine("Bye!");
---
+++ src/TodoApp.Domain/Phase1/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Domain.Phase1
+{
+    public record Todo(
+        int Id,
+        string Title,
+        DateOnly? DueDate,
+        bool IsDone = false
+    );
+}
