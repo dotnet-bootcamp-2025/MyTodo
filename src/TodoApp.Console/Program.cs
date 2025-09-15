6388af548ff1ccfc180ca52f64e99f197dcf46b0
Silvia Cruz <silvia.cruz@gmail.com>
2025-09-15 12:35:15 -0600

    Phase_1_CS_Syntax_Variables - FirstName LastName

--- src/TodoApp.Console/Program.cs
+++ src/TodoApp.Console/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using TodoApp.Application.DTOs;
 using TodoApp.Application.Interfaces;
+using TodoApp.Domain;
 using TodoApp.Infrastructure;
 
 var services = new ServiceCollection();
@@ -69,7 +70,6 @@ while (!exit)
     }
 }
 
-<<<<<<< Updated upstream
 async Task ViewAllTodos()
 {
     var todos = await todoService.GetAllTodosAsync();
@@ -273,62 +273,6 @@ async Task ResetTodo()
 
     Console.WriteLine("\nPress any key to continue...");
     Console.ReadKey();
-=======
-Console.WriteLine("Bye!");
 
-/*using TodoApp.Domain;
-using TodoApp.Infrastructure;*/
-
-//namespace TodoApp.Infrastructure;//
-
-public class InMemoryTodoStore
-{
-    private readonly List<Todo> _items = new();
-    private readonly Dictionary<int, Todo> _byId = new();
-    private int _nextId = 1;
-
-    public IReadOnlyList<Todo> All => _items;
-
-    public Todo Add(string title, DateOnly? dueDate = null)
-    {
-        var todo = new Todo(_nextId++, title, dueDate, false);
-        _items.Add(todo);
-        _byId[todo.Id] = todo;
-        return todo;
-    }
-
-    public IEnumerable<Todo> List() => _items;
-
-    public bool TryGet(int id, out Todo todo) => _byId.TryGetValue(id, out todo);
-
-    public bool Complete(int id)
-    {
-        if (!_byId.TryGetValue(id, out var existing)) return false;
-
-        var updated = existing with { IsDone = true };
-
-        var index = _items.FindIndex(t => t.Id == id);
-        if (index >= 0) _items[index] = updated;
-
-        _byId[id] = updated;
-        return true;
-    }
-
-    public bool Delete(int id)
-    {
-        if (!_byId.Remove(id)) return false;
-        var removed = _items.RemoveAll(t => t.Id == id) > 0;
-        return removed;
-    }
-
-    /// <summary>
-    /// Seed some sample tasks for demos.
-    /// </summary>
-    public void Seed()
-    {
-        Add("Buy milk", DateOnly.FromDateTime(DateTime.Today.AddDays(1)));
-        Add("Finish Module 1 notes", DateOnly.FromDateTime(DateTime.Today.AddDays(2)));
-        Add("Call the mechanic");
-    }
->>>>>>> Stashed changes
+    Console.WriteLine("Bye!");
 }
---
+++ src/TodoApp.Domain/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Domain;
+
+public record Todo(
+    int Id,
+    string Title,
+    DateOnly? DueDate,
+    bool IsDone = false
+);
